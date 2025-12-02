cb64caa36efd861754a2a74dc3de3779866b5c9b
Eid Jbehe <eidjbehe@gmail.com>
2025-12-02 23:03:24 +0200

    add single page aplication

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Navbar from './assets/components/navbar/Navbar.jsx'
+import Footer from './assets/components/footer/Footer.jsx'
+import { Route, Routes } from 'react-router-dom'
+import Users from './assets/pages/user/Users.jsx'
+import AddUser from './assets/pages/user/AddUser.jsx'
+import User from './assets/pages/user/User.jsx'
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+    <Navbar />
+    <Routes>
+      <Route path="/users" element={<Users />} />
+      <Route path="/addUser" element={<AddUser />} />
+      <Route path="/user" element={<User />} />
+    </Routes>
+    <Footer />
+    
+    
+    </>
   )
 }
