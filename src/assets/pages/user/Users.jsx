2da604bed44d9b506de389f3a2b58de5639354b2
Eid Jbehe <eidjbehe@gmail.com>
2025-12-03 22:33:25 +0200

    add delete of user

--- src/assets/pages/user/Users.jsx
+++ src/assets/pages/user/Users.jsx
@@ -5,6 +5,19 @@ import { Link } from "react-router-dom";
 
 export default function Users() {
   const { data, isError, isLoading } = useFetch("users");
+  const deleteUser=async(id)=>{
+    try{
+      const response=await axios.delete(`${import.meta.env.VITE_BURL}/users/${id}`);
+      console.log(response.data);
+    }
+    catch(error){
+      console.log(error.message);
+    }
+    finally{
+      
+    }
+    
+  }
 
   if (isError) {
     return <div className="text-danger">{isError}</div>;
@@ -30,10 +43,11 @@ export default function Users() {
                 <td>
                   <h3>{user.name}</h3>
                 </td>
-                <td>
+                <td className="d-flex gap-2">
                   <Link to={`/users/${user.id}`} className="btn btn-outline-success">
                     Details
                   </Link>
+                  <button onClick={()=>{deleteUser(user.id)}} className="btn btn-outline-danger">Delete</button>
                 </td>
               </tr>
             );
